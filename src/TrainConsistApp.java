809b4d0899f04871cd175fa05a3c3c025313c16d
Christopher Justin <christopherwilsonjust@gmail.com>
2026-04-11 20:19:56 +0530

    UC15 - Safe Cargo Assignment Using try-catch-finally

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,44 +1,44 @@
-class InvalidCapacityException extends Exception {
-    public InvalidCapacityException(String message) {
+class UnsafeCargoException extends Exception {
+    public UnsafeCargoException(String message) {
         super(message);
     }
 }
 
-class PassengerBogie {
+class GoodsBogie {
     private String bogieType;
-    private int seatCapacity;
+    private String cargoType;
 
-    public PassengerBogie(String bogieType, int seatCapacity) throws InvalidCapacityException {
-        if (seatCapacity <= 0) {
-            throw new InvalidCapacityException("Capacity must be greater than zero");
-        }
+    public GoodsBogie(String bogieType) {
         this.bogieType = bogieType;
-        this.seatCapacity = seatCapacity;
+    }
+
+    public void assignCargo(String cargoType) throws UnsafeCargoException {
+        if (bogieType.equalsIgnoreCase("Rectangular") && cargoType.equalsIgnoreCase("Petroleum")) {
+            throw new UnsafeCargoException("Unsafe cargo assignment: Petroleum cannot be assigned to Rectangular bogie");
+        }
+        this.cargoType = cargoType;
+        System.out.println("Cargo assigned successfully");
     }
 
     public String getBogieType() {
         return bogieType;
     }
 
-    public int getSeatCapacity() {
-        return seatCapacity;
+    public String getCargoType() {
+        return cargoType;
     }
 }
 
 public class TrainConsistApp {
     public static void main(String[] args) {
-        try {
-            PassengerBogie bogie1 = new PassengerBogie("Sleeper", 72);
-            System.out.println("Passenger Bogie Created Successfully");
-            System.out.println("Bogie Type: " + bogie1.getBogieType());
-            System.out.println("Seat Capacity: " + bogie1.getSeatCapacity());
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
 
-            PassengerBogie bogie2 = new PassengerBogie("AC Chair", 0);
-            System.out.println("Passenger Bogie Created Successfully");
-            System.out.println("Bogie Type: " + bogie2.getBogieType());
-            System.out.println("Seat Capacity: " + bogie2.getSeatCapacity());
-        } catch (InvalidCapacityException e) {
+        try {
+            bogie.assignCargo("Petroleum");
+        } catch (UnsafeCargoException e) {
             System.out.println("Exception: " + e.getMessage());
+        } finally {
+            System.out.println("Cargo assignment process completed");
         }
     }
 }
